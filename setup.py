7f779809522f7071808f9581cb37fc806f374018
williams824 <77971091+williams824@users.noreply.github.com>
2022-05-09 20:37:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-  name = 'GracefulKiller',
+  name = 'graceful_killer',
   version = '0.4.6',
   license = 'MIT',
   author = 'Maxim Toropov',
